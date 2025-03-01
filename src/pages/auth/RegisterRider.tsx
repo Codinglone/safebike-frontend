18b59f9158280109a53c498fddd1dfd0d8f66a64
Codinglone <codinglone@gmail.com>
2025-03-01 13:16:48 +0200

    Bug fix

--- src/pages/auth/RegisterRider.tsx
+++ src/pages/auth/RegisterRider.tsx
@@ -1,4 +1,3 @@
-// src/pages/auth/RegisterRider.tsx
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Formik, Form, Field } from 'formik';
@@ -13,9 +12,9 @@ const RegisterSchema = Yup.object().shape({
   firstName: Yup.string().required('First name is required'),
   lastName: Yup.string().required('Last name is required'),
   email: Yup.string().email('Invalid email').required('Email is required'),
-  phone: Yup.string().required('Phone number is required'),
+  phoneNumber: Yup.string().required('Phone number is required'),
   plateNumber: Yup.string().required('Plate number is required'),
-  licenseNumber: Yup.string().required('License number is required'),
+  ResidencyAddress: Yup.string().required('Residency address is required'),
   password: Yup.string()
     .min(8, 'Password must be at least 8 characters')
     .required('Password is required'),
@@ -29,7 +28,7 @@ const RegisterRider = () => {
   const navigate = useNavigate();
   const [isSubmitting, setIsSubmitting] = useState(false);
   
-  const handleSubmit = async (values: { firstName: string; lastName: string; email: string; phone: string; plateNumber: string; licenseNumber: string; password: string; confirmPassword: string; }) => {
+  const handleSubmit = async (values) => {
     if (values.password !== values.confirmPassword) {
       toast({
         title: 'Passwords do not match',
@@ -54,7 +53,7 @@ const RegisterRider = () => {
       });
       
       navigate('/auth/login');
-    } catch (error: any) {
+    } catch (error) {
       toast({
         title: 'Registration failed',
         description: error.response?.data?.error || 'An error occurred',
@@ -75,9 +74,9 @@ const RegisterRider = () => {
           firstName: '',
           lastName: '',
           email: '',
-          phone: '',
+          phoneNumber: '',
           plateNumber: '',
-          licenseNumber: '',
+          ResidencyAddress: '',
           password: '',
           confirmPassword: ''
         }}
@@ -87,49 +86,49 @@ const RegisterRider = () => {
         {({ errors, touched }) => (
           <Form>
             <VStack spacing={4}>
-              <FormControl isInvalid={!!errors.firstName && touched.firstName}>
+              <FormControl isInvalid={!!errors.firstName && !!touched.firstName}>
                 <FormLabel>First Name</FormLabel>
                 <Field as={Input} id="firstName" name="firstName" />
                 <FormErrorMessage>{errors.firstName}</FormErrorMessage>
               </FormControl>
               
-              <FormControl isInvalid={!!errors.lastName && touched.lastName}>
+              <FormControl isInvalid={!!errors.lastName && !!touched.lastName}>
                 <FormLabel>Last Name</FormLabel>
                 <Field as={Input} id="lastName" name="lastName" />
                 <FormErrorMessage>{errors.lastName}</FormErrorMessage>
               </FormControl>
               
-              <FormControl isInvalid={!!errors.email && touched.email}>
+              <FormControl isInvalid={!!errors.email && !!touched.email}>
                 <FormLabel>Email</FormLabel>
                 <Field as={Input} id="email" name="email" type="email" />
                 <FormErrorMessage>{errors.email}</FormErrorMessage>
               </FormControl>
               
-              <FormControl isInvalid={!!errors.phone && touched.phone}>
+              <FormControl isInvalid={!!errors.phoneNumber && !!touched.phoneNumber}>
                 <FormLabel>Phone Number</FormLabel>
-                <Field as={Input} id="phone" name="phone" />
-                <FormErrorMessage>{errors.phone}</FormErrorMessage>
+                <Field as={Input} id="phoneNumber" name="phoneNumber" />
+                <FormErrorMessage>{errors.phoneNumber}</FormErrorMessage>
               </FormControl>
               
-              <FormControl isInvalid={!!errors.plateNumber && touched.plateNumber}>
+              <FormControl isInvalid={!!errors.plateNumber && !!touched.plateNumber}>
                 <FormLabel>Motorcycle Plate Number</FormLabel>
                 <Field as={Input} id="plateNumber" name="plateNumber" />
                 <FormErrorMessage>{errors.plateNumber}</FormErrorMessage>
               </FormControl>
               
-              <FormControl isInvalid={!!errors.licenseNumber && touched.licenseNumber}>
-                <FormLabel>License Number</FormLabel>
-                <Field as={Input} id="licenseNumber" name="licenseNumber" />
-                <FormErrorMessage>{errors.licenseNumber}</FormErrorMessage>
+              <FormControl isInvalid={!!errors.ResidencyAddress && !!touched.ResidencyAddress}>
+                <FormLabel>Residency Address</FormLabel>
+                <Field as={Input} id="ResidencyAddress" name="ResidencyAddress" />
+                <FormErrorMessage>{errors.ResidencyAddress}</FormErrorMessage>
               </FormControl>
               
-              <FormControl isInvalid={!!errors.password && touched.password}>
+              <FormControl isInvalid={!!errors.password && !!touched.password}>
                 <FormLabel>Password</FormLabel>
                 <Field as={Input} id="password" name="password" type="password" />
                 <FormErrorMessage>{errors.password}</FormErrorMessage>
               </FormControl>
               
-              <FormControl isInvalid={!!errors.confirmPassword && touched.confirmPassword}>
+              <FormControl isInvalid={!!errors.confirmPassword && !!touched.confirmPassword}>
                 <FormLabel>Confirm Password</FormLabel>
                 <Field as={Input} id="confirmPassword" name="confirmPassword" type="password" />
                 <FormErrorMessage>{errors.confirmPassword}</FormErrorMessage>
